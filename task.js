3b81d94eab3703471e5d39e877af1bc54a43c923
Eugene Alforov <eugenalforov@gmail.com>
2017-09-30 14:10:45 +0300

    completed feature

--- task.js
+++ task.js
@@ -1,3 +1,4 @@
 var firstVariable = 'first commit';
 var variableForFirstFeature = 'first feature';
 var secondFeature = 'second feature';
+var secondFeature1 = 'another variable for second feature';

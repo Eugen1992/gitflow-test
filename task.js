14993ea1c3bffe6de8aac5ded199375031b04df2
Eugene Alforov <eugenalforov@gmail.com>
2017-10-02 12:12:01 +0300

    fix for second feature

--- task.js
+++ task.js
@@ -3,3 +3,4 @@ var changeInMaster = 0;
 var variableForFirstFeature = 'first feature';
 var secondFeature = 'second feature';
 var secondFeature1 = 'another variable for second feature';
+var secondFeature2 = 'third variable for second feature';

e285489d61ce1607fb915a9c0e576d235b87d025
Eugene Alforov <eugenalforov@gmail.com>
2017-10-02 12:13:17 +0300

    commit to master

--- task.js
+++ task.js
@@ -1,4 +1,5 @@
 var firstVariable = 'first commit';
+var changeInMaster = 0;
 var variableForFirstFeature = 'first feature';
 var secondFeature = 'second feature';
 var secondFeature1 = 'another variable for second feature';
